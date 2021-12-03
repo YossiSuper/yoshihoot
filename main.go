70969f212ed8dbbae44339fc9737f4ef9886f740
YossiSuper <naoki1023yos@yahoo.co.jp>
2021-12-03 22:45:11 +0900

    インデックスへのルート作成

--- main.go
+++ main.go
@@ -1,7 +1,77 @@
+// ---------------------------------------------------------------------
+/*
+
+   server.go
+
+                       Jun/11/2018
+*/
+// ---------------------------------------------------------------------
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io"
+	"net/http"
 
-func main() {
-	fmt.Printf("Yosshihoot!")
+	"github.com/labstack/echo"
+)
+
+type Template struct {
+	templates *template.Template
 }
+
+// ---------------------------------------------------------------------
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+// ---------------------------------------------------------------------
+// サイトで共通情報
+type ServiceInfo struct {
+	Title string
+}
+
+var serviceInfo = ServiceInfo{
+	"サイトのタイトル",
+}
+
+// ---------------------------------------------------------------------
+func main() {
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+
+	e := echo.New()
+
+	e.Renderer = t
+
+	e.Static("/", "public")
+
+	e.GET("/", func(c echo.Context) error {
+		return c.File("views/index.html")
+	})
+
+	e.GET("/page1", func(c echo.Context) error {
+		// テンプレートに渡す値
+
+		data := struct {
+			ServiceInfo
+			Content_a string
+			Content_b string
+			Content_c string
+			Content_d string
+		}{
+			ServiceInfo: serviceInfo,
+			Content_a:   "雨が降っています。",
+			Content_b:   "明日も雨でしょうか。",
+			Content_c:   "台風が近づいています。",
+			Content_d:   "Jun/11/2018",
+		}
+		return c.Render(http.StatusOK, "page1", data)
+	})
+
+	e.Logger.Fatal(e.Start(":80"))
+}
+
+// ---------------------------------------------------------------------
