d849fa80dad77977266159ba9361ef34b11c65bf
YossiSuper <naoki1023yos@yahoo.co.jp>
2021-12-12 15:18:09 +0900

    構造体追加

--- WebServer/structs/structs.go
+++ WebServer/structs/structs.go
@@ -1,11 +1,21 @@
 package structs
 
-type UserInfo struct {
+type User struct {
 	UUID string
 	Name string
 }
 
-type RoomInfo struct {
+type UserAPI struct {
+	User   User
+	Status APIStatus
+}
+
+type APIStatus struct {
+	Status  string
+	Message string
+}
+
+type Room struct {
 	UUID string
 	Name string
 	Quiz string
