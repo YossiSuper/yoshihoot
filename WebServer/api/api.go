7b1b3a3c81f55d24b62829ee211a346f382aee58
YossiSuper <naoki1023yos@yahoo.co.jp>
2021-12-17 15:22:46 +0900

    すでに使われている名前かチェック(1)

--- WebServer/api/api.go
+++ WebServer/api/api.go
@@ -1,20 +1,47 @@
 package api
 
 import (
+	"fmt"
+	"yoshihoot_WebServer/db"
 	"yoshihoot_WebServer/structs"
 	"yoshihoot_WebServer/uuid"
 )
 
+type User struct {
+	ID   int
+	Name string
+}
+
 //ログインAPI
 func LogIn(name string) structs.UserAPI {
+	var message string
+	var status string
+
+	//データーベース接続
+	db, err := db.SqlConnect()
+	//エラー処理
+	if err != nil {
+		message = "ServerError:DBに接続できませんでした。"
+		status = "Error"
+		fmt.Println(message)
+	} else {
+		message = "リクエストは正常に処理されました。"
+		status = "ok"
+		fmt.Println("DB接続成功")
+	}
+	user := structs.User{}
+	db.Where("name = ?", name).First(&user)
+
+	fmt.Println(user)
+
 	UserInfo := structs.UserAPI{
 		User: structs.User{
 			UUID: uuid.GenerateUUID(),
 			Name: name,
 		},
 		Status: structs.APIStatus{
-			Status:  "OK",
-			Message: "None",
+			Status:  status,
+			Message: status,
 		},
 	}
 	return UserInfo
